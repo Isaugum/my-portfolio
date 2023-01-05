2a7502a17885abcf38c389d66594735cf70be5b1
Isaugum <gorjup.domen95@gmail.com>
2023-01-05 14:28:33 +0100

    fixed script issues, added project buttons script

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,37 @@ console.log("connected");
 
 const projectButtons = document.querySelectorAll('.project-button');
 
-projectButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        console.log("clickclick");
-        btn.style.color = "red";
-    });
-})
+window.addEventListener("load", () => {
+    projectButtons.forEach(btn => {
+
+        //controlled hoover effect
+        btn.addEventListener('mouseover', () => {
+            if(btn.dataset.active !== "true") {
+                btn.style.color = "#707070";
+            }
+        });
+
+        btn.addEventListener('mouseout', () => {
+            if(btn.dataset.active !== "true") {
+                btn.style.color = "#3F3F3F";
+            }
+        });
+
+        //manage color and state change
+        btn.addEventListener('click', () => {
+    
+            projectButtons.forEach(btn => {
+                 if(btn.dataset.active === "true") {
+                     btn.dataset.active = "false";
+                     btn.style.color = "#3F3F3F";
+                 }
+             })
+     
+             btn.dataset.active = "true";
+             btn.style.color = "#86A4B8";
+         });
+    })
+
+    //set initial active button
+    projectButtons[0].style.color = "#86A4B8";
+});
