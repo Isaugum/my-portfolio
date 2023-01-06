b44ace3e17eec411efba119edf4b40974231e151
Isaugum <gorjup.domen95@gmail.com>
2023-01-06 14:51:05 +0100

    fixed intersection observer

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,33 @@
 console.log("connected");
 
+const navbar = document.querySelector('.navbar');
+const navBtns = document.querySelectorAll('.navbar > ul > li');
+const navTitle = document.querySelector('.navbar > h1');
+const landingText = document.querySelector('.landing-page-text');
 const projectButtons = document.querySelectorAll('.project-button');
+
+const aboutMeSection = document.querySelector('.cv-section');
+const aboutText = document.querySelector('.cv-section > .cv-text');
+const aboutSkills = document.querySelector('.cv-section > .cv-skills');
+
+const getSpecificPosition = (positionX, positionY) => {
+    window.scrollTo(positionX, positionY);
+}
+
+//INTERSECTION OBSERVER FOR ABOUT ME
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            let children = entry.target.children;
+            children[0].style.transform = 'translate(0, 0)';
+            children[0].style.opacity = '1';
+            children[1].style.transform = 'translate(0, 0)';
+            children[1].style.opacity = '1';
+            }
+        });
+}, { threshold: 0.3 })
+
+observer.observe(aboutMeSection);
 
 window.addEventListener("load", () => {
     projectButtons.forEach(btn => {
@@ -33,6 +60,26 @@ window.addEventListener("load", () => {
          });
     })
 
+    //MANUAL CLICK LISTENER FOR "ABOUT ME" to account for padding-top
+    navBtns[0].addEventListener("click", () => {
+        getSpecificPosition(0, 700);
+    });
+
     //set initial active button
     projectButtons[0].style.color = "#86A4B8";
+
+    //navbar animation
+    navbar.style.transform = "translate(0, 0)";
+    navbar.style.opacity = "1";
+
+    navBtns.forEach(btn => {
+        btn.style.transform = "translate(0, 0)";
+        btn.style.opacity = "1";
+    });
+
+    navTitle.style.opacity = "1";
+
+    //landing page text animation
+    landingText.style.transform = "translate(0, 0)";
+    landingText.style.opacity = "1";
 });
